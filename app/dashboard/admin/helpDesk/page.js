af052b741b982bc404e072f3cbc6278cf0c424c1
abcarso <lainalaire@gmail.com>
2025-03-04 15:47:28 -0500

    admin help desk and log pages added

---
+++ app/dashboard/admin/helpDesk/page.js
@@ -0,0 +1,10 @@
+"use client";
+import  React , { useState } from 'react';
+
+export default function HelpDesk() {
+    return (
+      <main>
+        <h1>Help Desk</h1>
+      </main>
+    );
+  }
---
+++ app/dashboard/admin/logs/page.js
@@ -0,0 +1,10 @@
+"use client";
+import  React , { useState } from 'react';
+
+export default function Logs() {
+    return (
+      <main>
+        <h1>Audit Logs</h1>
+      </main>
+    );
+  }
