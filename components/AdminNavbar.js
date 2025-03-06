2df89d0fced0d1a03b40e4c3611ad5283d90ecf3
abcarso <lainalaire@gmail.com>
2025-03-06 02:04:15 -0500

    Sponsor pages improved

--- components/AdminNavbar.js
+++ components/AdminNavbar.js
@@ -4,8 +4,9 @@ export default function AdminNavbar() {
       <nav className="navbar">
         <ul>
           <li><a href="/pages/admin">Dashboard</a></li>
-          <li><a href="/pages/admin/helpDesk">Help Desk</a></li>
           <li><a href="/pages/admin/logs">Audit Logs</a></li>
+          <li><a href="/pages/admin/helpDesk">Help Desk</a></li>
+          <li><a href="/pages/admin/sponsorPolicies">Sponsors</a></li>
           <li><a href="/logout">Logout</a></li>
         </ul>
       </nav>
--- components/SponsorNavbar.js
+++ components/SponsorNavbar.js
@@ -4,6 +4,9 @@ export default function SponsorNavbar() {
       <nav className="navbar">
         <ul>
           <li><a href="/pages/sponsor">Dashboard</a></li>
+          <li><a href="/pages/sponsor/drivers">My Drivers</a></li>
+          <li><a href="/pages/sponsor/applications">Driver Applications</a></li>
+          <li><a href="/pages/sponsor/reports">Reports</a></li>
           <li><a href="/logout">Logout</a></li>
         </ul>
       </nav>
