55a38223dab9cdc31e5662baaa4fc08bb9fc3a2f
Anna Budreau <akbudreau@gmail.com>
2025-03-11 14:16:11 -0400

    adding storage stuff i think

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -47,5 +47,11 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     }
+  },
+  "storage": {
+    "team24ProfilePictures": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
